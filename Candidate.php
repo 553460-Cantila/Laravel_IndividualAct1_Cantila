fe8172324406bd573287e3b00c1405b03b6fa27c
553460-Cantila <j.cantila.553460@umindanao.edu.ph>
2026-03-26 23:30:29 +0800

    Delete Candidate.php

--- Candidate.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-class Candidate extends Model
-{
-    protected $table = 'candidates';
-    protected $fillable=['first_name', 'middle_name', 'last_name', 'gender', 'address', 'position', 'party'];
-}
